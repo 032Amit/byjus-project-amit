c7a8712e79581ba03c8373957a7e042fcf079b01
Amit032 <amitnit2k18@gmail.com>
2020-07-12 19:29:29 +0530

    2nd commit

---
+++ precache-manifest.348881264d25a044824bea00fb258ed6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "57e0fd1863e11a13b288",
+    "url": "/static/css/main.5d70e08d.chunk.css"
+  },
+  {
+    "revision": "57e0fd1863e11a13b288",
+    "url": "/static/js/main.57e0fd18.chunk.js"
+  },
+  {
+    "revision": "ce6700f68a9742ada2a9",
+    "url": "/static/js/1.ce6700f6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "299c7a58f60166129113b682a6fc0f8d",
+    "url": "/static/media/malePP.299c7a58.png"
+  },
+  {
+    "revision": "b8b8e4109fd729981627d6fe8b4d8d77",
+    "url": "/static/media/byjusLogo.b8b8e410.PNG"
+  },
+  {
+    "revision": "09031affe3b51a6950d03ea90b8c17f8",
+    "url": "/static/media/femalePP.09031aff.png"
+  },
+  {
+    "revision": "0ffcd2786d9c7162b5290232f4396a62",
+    "url": "/index.html"
+  }
+];
